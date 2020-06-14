a038e9c6fac2e9c0c53c69934ba16d1a426bdc03
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-14 01:45:46 -0300

    criado model article e criado relacionamento com model category

---
+++ articles/Article.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const Conn = require('../database/database');
+const Category = require('../categories/Category');
+
+const Article = Conn.define('articles', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+Category.hasMany(Article); // Relacionamento 1..*  - Cada categoria possui nenhum ou muitos artigos
+Article.belongsTo(Category); //Relacionamento 1..1 - Cada artigo possui apenas uma categoria
+
+Article.sync({force: false});
+
+module.exports = Article;
