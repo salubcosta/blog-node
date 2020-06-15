9aa68efca2dab40e267980ff362d231c2cd1b277
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-15 01:46:25 -0300

    add routes to new and save and loading common. Also, was included findall to index js file

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const slugify = require('slugify');
+const Category = require('./Category');
+
 const router = express.Router();
 
-router.get('/categories', (req, res)=>{
-    res.render('index');
+router.get('/admin/categories/new', (req, res)=>{
+    res.render('admin/categories/new');
 });
+
+router.post('/categories/save',(req, res)=>{
+    const title = req.body.title;
+
+    if(title != undefined){
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        })
+        .then(()=>{
+            console.log('created category!');
+            res.redirect('/');
+        })
+        .catch(err=>console.log(`Oh my Good Lord! Something wrong happened! ${err}`));
+
+    } else {
+        res.redirect('/admin/categories/new');
+    }
+});
+
+router.get('/admin/categories', (req, res)=>{
+    Category.findAll({raw: true})
+    .then((cat)=>{
+        res.render('admin/categories/index', { cat: cat});
+    }).catch(err=>console.log(`Sorry about this. Error: ${err}`));
+})
 
 module.exports = router;
