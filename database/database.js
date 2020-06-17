9d8a4d822b3deefec5aaddee63d4f73780922538
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-17 00:14:07 -0300

    include timezone -03:00 references brazil

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const Connection =  new Sequelize('blogdb', 'salumao', '123456', {
     host: 'localhost',
-    dialect: 'mariadb'
+    dialect: 'mariadb',
+    timezone: '-03:00'
 });
 
 module.exports = Connection;
