77dd40c49f639168924a718e614888e7b22838d7
Salumão Barbosa da Costa <salubcosta@gmail.com>
2020-06-18 02:59:17 -0300

    include description field to save

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,9 @@ const Article = require('./Article');
 const Slugify = require('slugify');
 
 router.get('/admin/articles', (req, res)=>{
-    res.render('admin/articles');
+    Article.findAll({include: [{model: Category}]}).then((articles)=>{
+        res.render('admin/articles', {articles: articles});
+    });
 })
 
 router.get('/admin/articles/new',(req, res)=>{
@@ -20,22 +22,24 @@ router.post('/articles/save', (req, res)=>{
     const title = req.body.title;
     const body = String(req.body.body);
     const category = req.body.category;
+    const description = req.body.description;
     
     if(isNaN(category)){
-        res.redirect('/');
+        res.redirect('/admin/articles');
         return;
     }
-    if(String(title).length < 10){
-        res.redirect('/');
+    if(String(title).length < 10 || String(description).length < 10){
+        res.redirect('/admin/articles');
         return;
     } 
     if(body.length < 200){
-        res.redirect('/');
+        res.redirect('/admin/articles');
         return;
     }
     
     Article.create({
         title: title,
+        description: description,
         slug: Slugify(title),
         body: body,
         categoryId: category
@@ -43,5 +47,20 @@ router.post('/articles/save', (req, res)=>{
         res.redirect('/admin/articles');
     });
 })
+
+router.post('/article/delete', (req, res)=>{
+    const data = JSON.parse(req.body.id);
+    if(data.id != undefined){
+        if(!isNaN(data.id)){
+            Article.destroy({where: {id: data.id}}).then(()=>{
+                res.redirect('/admin/articles');
+            });
+        } else {
+            res.redirect('/admin/articles');    
+        }
+    } else {
+        res.redirect('/admin/articles');
+    }
+});
 
 module.exports = router;
